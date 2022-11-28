12489837da736841671d81093f44f7798e2e1208
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-11-28 18:03:56 +0200

    added form validation

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ new Swiper('.swiper',{
       clickable: true 
     },
 });
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
