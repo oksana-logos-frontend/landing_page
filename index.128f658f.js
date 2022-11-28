540c2ff5b78e473ca99face85425800e2cd5e9b8
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-11-28 15:29:55 +0200

    Updates

--- index.128f658f.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{observer:!0,pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.128f658f.js.map
---
+++ index.js
@@ -0,0 +1,21 @@
+// import Swiper, { Pagination } from 'swiper';
+// import 'swiper/swiper-bundle.css';
+
+// Swiper.use([Pagination]);
+// const swiper = new Swiper('.swiper', {
+//   observer: true,
+  
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true 
+//   },
+// });
+
+new Swiper('.swiper',{
+  observer: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true 
+    },
+})
